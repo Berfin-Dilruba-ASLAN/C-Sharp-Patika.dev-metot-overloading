0d89227f08348d50777f538af0c4ba218f15f059
Berfin Dilruba ASLAN <98692629+Berfin-Dilruba-ASLAN@users.noreply.github.com>
2022-08-15 09:17:55 +0300

    Add files via upload

---
+++ metot_overloading/metot_overloading/metot_overloading/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace metot_overloading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string girilenSayi = "1905";
+            int sayimiz;
+            bool sonuc = int.TryParse(girilenSayi, out sayimiz);
+            if (sonuc)
+            {
+                Console.WriteLine("Dönüşüm başarılı.");
+                Console.WriteLine("Sayı: {0}", sayimiz);
+            }
+            else
+            {
+                Console.WriteLine("Dönüşüm başarısız.");
+            }
+        }
+    }
+}
